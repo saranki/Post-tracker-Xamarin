61afa6a531d3027f09a8b150903515f4bd6dc082
Saranki M <m.saranki@gmail.com>
2018-09-16 23:14:40 +0530

    Implemented Comment List page

--- EnterpriseApp/CommentPage.xaml.cs
+++ EnterpriseApp/CommentPage.xaml.cs
@@ -1,27 +1,97 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using EnterpriseApp.Data;
+using EnterpriseApp.Net;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
 namespace EnterpriseApp
 {
-	[XamlCompilation(XamlCompilationOptions.Compile)]
-	public partial class CommentPage : ContentPage
-	{
-        private Post post;
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class CommentPage : ContentPage
+    {
+        public ObservableCollection<Comment> CommentList { get; }
 
-        public CommentPage ()
-		{
-			InitializeComponent ();
-		}
+        DataRetriever dataRetriever;
+        public int? authorId;
 
-        public CommentPage(Post post)
+        public CommentPage()
         {
-            this.post = post;
+            InitializeComponent();
+        }
+
+        public CommentPage(Post post) : this()
+        {
+            BindingContext = post;
+            authorId = post.UserId;
+
+            Title = "Comments";
+
+            CommentList = new ObservableCollection<Comment>();
+            dataRetriever = new DataRetriever();
+
+            CommentsLoaderIndicator.IsRunning = true;
+            CommentsLoaderIndicator.IsVisible = true;
+
+            CommentListView.ItemsSource = CommentList;
+
+            CommentListView.ItemSelected += CommentListView_ItemSelected;
+
+            LoadComments(post.Id);
+
+            AddToolbarItem();
+        }
+
+        private void AddToolbarItem()
+        {
+            var toolBarItem = new ToolbarItem
+            {
+                Icon = "@drawable/author.png"
+            };
+
+            toolBarItem.Clicked += OnAuthorIconClicked;
+            ToolbarItems.Add(toolBarItem);
+        }
+
+        private void OnAuthorIconClicked(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new ProfilePage(authorId));
+        }
+
+        private async void LoadComments(int? id)
+        {
+            CommentList.Clear();
+
+            List<Comment> comments = await dataRetriever.GetCommentAsync(id);
+
+            foreach (Comment c in comments)
+            {
+                CommentList.Add(c);
+
+                if (CommentList.Count > 0)
+                {
+                    CommentsLoaderIndicator.IsRunning = false;
+                    CommentsLoaderIndicator.IsVisible = false;
+                }
+            }
+        }
+
+        private void CommentListView_ItemSelected(object sender, SelectedItemChangedEventArgs e)
+        {
+            if (e.SelectedItem != null)
+            {
+                Comment comment = e.SelectedItem as Comment;
+                string mailto = "mailto:" + comment.Email;
+
+                Device.OpenUri(new Uri(mailto));
+            }
+
+            //Clear Selection
+            CommentListView.SelectedItem = null;
         }
     }
 }
