f17dac7f0067131df163c5028420d08c8f9d58de
Saranki M <m.saranki@gmail.com>
2018-09-16 23:16:17 +0530

    Added icon to the app and changed the app name

--- EnterpriseApp.Android/MainActivity.cs
+++ EnterpriseApp.Android/MainActivity.cs
@@ -4,7 +4,7 @@ using Android.OS;
 
 namespace EnterpriseApp.Droid
 {
-    [Activity(Label = "Post Tracker", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    [Activity(Label = "Post Tracker", Icon = "@drawable/postIcon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle savedInstanceState)
